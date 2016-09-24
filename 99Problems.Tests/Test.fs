f35caf164567ba54ff31416b72b5fcc51ce73dac
fsharpcorner <hezzahir@hotmail.com>
2016-09-24 18:45:35 +0100

    Problem1

---
+++ 99Problems.Tests/Test.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+
+open FsUnit
+open NUnit.Framework
+open Main
+
+[<Test>]
+let mylast_1() =
+    myLast [1;2;3;4] |> should equal (Some 4)
+[<Test>]
+let mylast_2() =
+    myLast [] |> should equal None
+
+
---
+++ 99_Problems_FSharp/99Problems.fs
@@ -0,0 +1,10 @@
+﻿//namespace _99_Problems_FSharp
+module Main
+
+let myLast l =
+    match l with
+    | [] -> None
+    | _ -> l |> List.rev |> List.head |> Some
+
+
+
